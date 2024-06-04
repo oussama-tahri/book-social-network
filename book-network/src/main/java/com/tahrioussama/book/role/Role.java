ab31baf1df9244f6a935305cca6974056eaa13ac
TAHRI <oussama.tahri@capgemini.com>
2024-06-04 15:43:44 +0100

    implementing security logic v1

---
+++ book-network/src/main/java/com/tahrioussama/book/role/Role.java
@@ -0,0 +1,39 @@
+package com.tahrioussama.book.role;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.tahrioussama.book.user.User;
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.aop.target.LazyInitTargetSource;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Entity
+@EntityListeners(AuditingEntityListener.class)
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Role {
+    @Id
+    @GeneratedValue
+    private Integer id;
+    @Column(unique = true)
+    private String name;
+    @ManyToMany(mappedBy = "roles")
+    @JsonIgnore
+    private List<User> users;
+
+    @CreatedDate
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdDate;
+    @LastModifiedDate
+    @Column(insertable = false)
+    private LocalDateTime lastModifiedDate;
+}
